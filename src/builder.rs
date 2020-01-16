43a946ce5050b5c67d96b9e4a9922b8faac384ec
bakape <bakape@gmail.com>
2020-01-16 23:06:40 +0200

    Add collection values

--- src/builder.rs
+++ src/builder.rs
@@ -1,7 +1,11 @@
+use chrono::{Date, DateTime, Duration};
 use serde::{Deserialize, Serialize};
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+use std::net::IpAddr;
+use uuid::Uuid;
 
 // Select statement builder
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct SelectBuilder {
 	// Source table or statement
 	// TODO: enum
@@ -12,7 +16,7 @@ pub struct SelectBuilder {
 	columns: Vec<Column>,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone, Ord, PartialOrd, Eq, PartialEq)]
 struct Column {
 	name: String,
 	alias: Option<String>,
@@ -51,7 +55,7 @@ pub fn select(
 macro_rules! build_value_enum {
     ($($variant:tt)+) => {
 		// Supported value types usable in the query builder
-		#[derive(Serialize, Deserialize, Clone)]
+		#[derive(Serialize, Deserialize, Clone, Ord, PartialOrd, Eq, PartialEq)]
 		#[allow(non_camel_case_types)]
 		pub enum Value {
 			// Lack of value
@@ -71,47 +75,165 @@ macro_rules! build_value_enum {
 			)+
 
 			// Constant value
-			Vec_u8(Vec<u8>),
+			f32(F32),
+
+			// Constant value
+			f64(F64),
+
+			// Constant value
+			UUID([u8; 16]),
+
+			// Constant vector of constant values
+			Vec(Vec<Value>),
+
+			// Constant BTreeSet of constant values
+			Set(BTreeSet<Value>),
+
+			// Constant BTreeMap of constant values
+			Map(BTreeMap<Value, Value>),
 		}
 
-		$(
-			impl From<$variant> for Value {
-				fn from(v: $variant) -> Self {
-					Self::$variant(v)
-				}
-			}
-		)+
+		$( impl_value_conversion! {$variant} )+
     };
 }
 
+impl Default for Value {
+	fn default() -> Self {
+		Self::Null
+	}
+}
+
+macro_rules! impl_value_conversion {
+	($type:ty, $variant:ident) => {
+		impl_value_conversion! {
+			$type,
+			|v: $type| Self::$variant(v.into())
+		}
+	};
+	($type:ty, $convert:expr) => {
+		impl From<$type> for Value {
+			fn from(v: $type) -> Self {
+				$convert(v)
+			}
+		}
+	};
+	($type:tt) => {
+		impl_value_conversion! {$type, $type}
+	};
+}
+
 build_value_enum! {
+	bool
+
 	i8 i16 i32 i64 i128 isize
 	u8 u16 u32 u64 u128
-	f32 f64
+
 	char
-	bool
 	String
 
-	// TODO: More types (see Postgres for inspiration)
+	IpAddr
+
+	// Duration
+	// Date
+	// DateTime
 }
 
-impl From<&str> for Value {
-	fn from(v: &str) -> Self {
-		Self::Column(v.into())
+macro_rules! impl_wrapper_tuple {
+	($tuple:tt, $inner:tt) => {
+		define_tuple! {$tuple, $inner}
+
+		impl From<$inner> for $tuple {
+			fn from(v: $inner) -> Self {
+				Self(v)
+			}
+		}
+
+		impl Into<$inner> for $tuple {
+			fn into(self) -> $inner {
+				self.0
+			}
+		}
+
+		impl std::cmp::PartialEq for $tuple {
+			fn eq(&self, other: &$tuple) -> bool {
+				self.0.to_le_bytes() == other.0.to_le_bytes()
+			}
+		}
+
+		impl std::cmp::Eq for $tuple {}
+
+		impl std::cmp::PartialOrd for $tuple {
+			fn partial_cmp(
+				&self,
+				other: &$tuple,
+			) -> Option<std::cmp::Ordering> {
+				self.0.to_le_bytes().partial_cmp(&other.0.to_le_bytes())
+			}
+		}
+
+		impl std::cmp::Ord for $tuple {
+			fn cmp(&self, other: &$tuple) -> std::cmp::Ordering {
+				self.0.to_le_bytes().cmp(&other.0.to_le_bytes())
+			}
+		}
+	};
+}
+
+macro_rules! define_tuple {
+	($ident:ident, $inner:tt) => {
+		// Wrapper for hashing using as a hash collection key
+		#[derive(Serialize, Deserialize, Clone)]
+		pub struct $ident($inner);
+	};
+}
+
+impl_wrapper_tuple! {F32, f32}
+impl_wrapper_tuple! {F64, f64}
+
+impl_value_conversion! {&str, Column}
+impl_value_conversion! {SelectBuilder, Expression}
+
+impl From<Uuid> for Value {
+	fn from(v: Uuid) -> Self {
+		Self::UUID(*v.as_bytes())
 	}
 }
 
-impl From<SelectBuilder> for Value {
-	fn from(v: SelectBuilder) -> Self {
-		Self::Expression(v)
-	}
+macro_rules! impl_value_from_linear {
+	($container:ident, $variant:ident) => {
+		impl<T> From<$container<T>> for Value
+		where
+			T: Into<Value>,
+		{
+			fn from(v: $container<T>) -> Self {
+				Value::$variant(v.into_iter().map(|v| v.into()).collect())
+			}
+		}
+	};
 }
 
-impl From<Vec<u8>> for Value {
-	fn from(v: Vec<u8>) -> Self {
-		Self::Vec_u8(v)
-	}
+impl_value_from_linear! {Vec, Vec}
+impl_value_from_linear! {BTreeSet, Set}
+impl_value_from_linear! {HashSet, Set}
+
+macro_rules! impl_value_from_map {
+	($container:ident, $variant:ident) => {
+		impl<K, V> From<$container<K, V>> for Value
+		where
+			K: Into<Value> + Eq + PartialEq,
+			V: Into<Value>,
+		{
+			fn from(v: $container<K, V>) -> Self {
+				Value::Map(
+					v.into_iter().map(|(k, v)| (k.into(), v.into())).collect(),
+				)
+			}
+		}
+	};
 }
+
+impl_value_from_map! {BTreeMap, Map}
+impl_value_from_map! {HashMap, Map}
 
 #[derive(Serialize, Deserialize, Clone)]
 pub enum Comparator {
@@ -236,7 +358,8 @@ mod tests {
 	#[allow(unused)]
 	fn filter_construction() -> Result<(), String> {
 		!(Filter::new("user", Comparator::Eq, 20_u64)
-			+ Filter::new("user", Comparator::Eq, select("id", &["article"])?));
+			+ Filter::new("user", Comparator::Eq, select("id", &["article"])?))
+			| Filter::new("bucket", Comparator::Eq, vec![1_i32]);
 		Ok(())
 	}
 }
